31365f13d5f2a4e0061c884ee43205de231fa3a6
jmendo15 <72280816+jmendo15@users.noreply.github.com>
2023-09-18 12:36:17 -0700

    Update alt_palindrome_permutation.js

--- alt_palindrome_permutation.js
+++ alt_palindrome_permutation.js
@@ -22,6 +22,21 @@ function checkMaxOneOdd(countMap) {
     return true; //that it is a permutation
 }
 
+//Map, HashMap, Dictionary
+// data structure: key alue
+// -unique key
+// -value
+// {"a": 1, "b": 2, "c": 3}
+// {"a": 3, "b": 2, "c": 1}
+// phrase[a] = 3
+// Map 
+// -keys unordered
+// -key can be anything
+// -get is slower than dictionary
+// Dictionary 
+// -keys are ordered
+// -key must be a character or a string
+
 function buildCharFrequencyTable(phrase) {
     var mappy = new Map();
     //for each char in string
@@ -33,3 +48,6 @@ function buildCharFrequencyTable(phrase) {
         }
     }
 }
+
+console.log(permPalindrome("tacocat");
+console.log(permPalindrome("cot a palindrome"));
