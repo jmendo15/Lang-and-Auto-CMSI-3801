1d81a7e2dba250e5e6aaa8bd8751bf6a846fcc82
jmendo15 <72280816+jmendo15@users.noreply.github.com>
2023-10-04 12:46:59 -0700

    Update database.py

--- database.py
+++ database.py
@@ -8,6 +8,28 @@ url = "https://en.wikipedia.org/wiki/List_of_Game_Boy_games"
 browser = mechanicalsoup.StatefulBrowser()
 browser.open(url)
 
-td = browser.page.find_all("td")
+tables = browser.page.find_all("table")
+td = tables[1].find_all("td")
+game_data = [value.text.strip() for value in td]
 
-print()
+print(game_data)
+
+# parse the data 1/ mechanical soup
+
+column_names = ["Title",
+                "Developers",
+                "Publishers",
+                "Release_Japan",
+                "Release_NorthAmerica",
+                "Release_PALregion"
+                ]
+
+# load data into a DataFrame w/ pandas
+# this gives our data a tabular structure
+
+dictionary = {}
+
+game_data[0:][::6]
+game_data[1:][::6]
+
+# load data from DataFrame into SQLite database
